d00cebfbaed849ca5dbacc994f0472d9f66143c3
@hexlet/cli <support@hexlet.io>
2022-11-20 12:12:03 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -6,13 +6,17 @@ import java.util.Map;
 // BEGIN
 class App {
     public static void main(String[] args) {
-        String sentence = "the java is the best programming language java";
-        Map<String, Integer> wordsCount = App.getWordCount(sentence);
-        String result = App.toString(wordsCount);
+        String sentence = "";
+        Map<String, Integer> wordsCount = getWordCount(sentence);
+        String result = toString(wordsCount);
         System.out.println(result);
     }
     public static Map<String, Integer> getWordCount(String text) {
         Map<String, Integer> wordsCount = new HashMap<>();
+        if (text.equals("")) {
+            return wordsCount;
+        }
+
         String[] result = text.split(" ");
         int[] count = new int[result.length];
         int i = -1;
@@ -25,10 +29,14 @@ class App {
             }
             wordsCount.put(str, count[i]);
         }
+
         return wordsCount;
     }
 
     public static String toString(Map<String, Integer> words) {
+        if (words.isEmpty()) {
+            return words.toString();
+        }
         StringBuilder result = new StringBuilder("{");
         for (Map.Entry<String, Integer> word : words.entrySet()) {
             result.append("\n  ").append(word.getKey()).append(": ").append(word.getValue());
