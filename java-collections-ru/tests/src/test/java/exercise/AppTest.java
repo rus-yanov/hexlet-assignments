5b382a6c0ce79a1c36490b4fe9b732192b45d248
@hexlet/cli <support@hexlet.io>
2022-11-22 15:20:57 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -1,6 +1,5 @@
 package exercise;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -13,10 +12,10 @@ class AppTest {
     @Test
     void testTake() {
         // BEGIN
-        List <Integer> expected = new ArrayList<>(Arrays.asList(1, 11, 21, 31));
-        List <Integer> elements = new ArrayList<>(Arrays.asList(1, 11, 21, 31, 41, 51));
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 11, 21, 31));
+        List<Integer> elements = new ArrayList<>(Arrays.asList(1, 11, 21, 31, 41, 51));
         int count = 4;
-        List <Integer> actual = App.take(elements,count);
+        List<Integer> actual = App.take(elements,count);
         Assertions.assertEquals(expected, actual);
 
         // END
