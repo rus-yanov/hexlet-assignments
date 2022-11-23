8430541181d2c32a65bec7fd9fcc048d403a39c1
Rustam Akhmedzianov <ahmedzyanov.r.1997@gmail.com>
2022-11-23 15:57:58 +0300

    update

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -6,45 +6,24 @@ import java.util.Map.Entry;
 // BEGIN
 public class App {
     public static void main(String[] args) {
-        List<Map> books = new ArrayList<>();
 
-        Map<String, String> book1 = new HashMap<>(
-                Map.of("title", "Cymbeline", "author", "Shakespeare", "year", "1611")
-        );
-        Map<String, String> book2 = new HashMap<>(
-                Map.of("title", "Book of Fooos", "author", "FooBar", "year", "1111")
-        );
-        Map<String, String> book3 = new HashMap<>(
-                Map.of("title", "The Tempest", "author", "Shakespeare", "year", "1611")
-        );
-        Map<String, String> book4 = new HashMap<>(
-                Map.of("title", "Book of Foos Barrrs", "author", "FooBar", "year", "2222")
-        );
-        Map<String, String> book5 = new HashMap<>(
-                Map.of("title", "Still foooing", "author", "FooBar", "year", "3333")
-        );
+    public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map<String, String> where) {
+        List<Map<String, String>> result = new ArrayList<>();
 
-        books.add(book1);
-        books.add(book2);
-        books.add(book3);
-        books.add(book4);
-        books.add(book5);
-
-        Map<String, String> where = new HashMap<>(Map.of("author", "Shakespeare", "year", "1611"));
-
-        List<Map> result = App.findWhere(books, where);
-
-        System.out.println(result);
-    }
-
-    public static List<Map> findWhere(List<Map> books, Map<String, String> where) {
-        List<Map> result = new ArrayList<>();
-
-        for (Map book : books) {
-            for (Map.Entry<String, String> item : where.entrySet()) {
-                if (book.containsKey(item.getKey())) {
-                    result.add(book);
+        for (Map<String, String> book : books) {
+            boolean check = false;
+            for (Map.Entry<String, String> criteria : where.entrySet()) {
+                if (book.containsKey(criteria.getKey())) {
+                    if (book.containsValue((criteria.getValue()))) {
+                        check = true;
+                    } else {
+                        check = false;
+                        break;
+                    }
                 }
+            }
+            if (check) {
+                result.add(book);
             }
         }
         return result;
