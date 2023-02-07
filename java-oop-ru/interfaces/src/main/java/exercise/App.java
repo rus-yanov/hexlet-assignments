268477dc044449c703ad4e624da647fbb2aec862
@hexlet/cli <support@hexlet.io>
2023-02-07 13:08:08 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 // BEGIN
 public class App {
-    public ArrayList<String> buildApartmentList(List<Home> list, int num) {
+    public static ArrayList<String> buildApartmentList(List<Home> list, int num) {
         List<String> result = new ArrayList<>();
         for (int i = 0; i < num; i++) {
             result.add(list.get(i).toString());
