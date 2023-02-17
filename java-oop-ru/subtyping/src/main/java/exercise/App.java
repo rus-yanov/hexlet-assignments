f1581cf956e52f190abb77882508802433c41173
@hexlet/cli <support@hexlet.io>
2023-02-17 17:40:14 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -3,17 +3,15 @@ package exercise;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.HashMap;
+import java.util.HashSet;
 
 // BEGIN
 public class App {
     public static void swapKeyValue(KeyValueStorage storage) {
-        Map<String, String> map = storage.toMap();
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            String value = entry.getValue();
-            String key = entry.getKey();
-            map.put(value, key);
-            map.remove(key);
+        Set<String> keys = new HashSet<>(storage.toMap().keySet());
+        for (String key : keys) {
+            storage.set(data.get(key, ""), key);
+            storage.unset(key);
         }
     }
 }
