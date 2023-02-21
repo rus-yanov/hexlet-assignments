b1593bffe549baa7ba334b6ff6387632a12bf82e
@hexlet/cli <support@hexlet.io>
2023-02-21 11:13:18 +0300

    submit java-oop-ru/inheritance

--- java-oop-ru/inheritance/src/main/java/exercise/SingleTag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/SingleTag.java
@@ -3,5 +3,10 @@ package exercise;
 import java.util.Map;
 
 // BEGIN
-public class SingleTag extends Tag;
+public class SingleTag extends Tag{
+
+    SingleTag(String data, Map<String, String> map) {
+        super(data, map);
+    }
+}
 // END
