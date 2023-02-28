14d0364c08db51d2c699ed86e7084f01327b2117
@hexlet/cli <support@hexlet.io>
2023-02-28 15:48:21 +0300

    submit java-oop-ru/code-generation

--- java-oop-ru/code-generation/src/main/java/exercise/App.java
+++ java-oop-ru/code-generation/src/main/java/exercise/App.java
@@ -5,7 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 
 // BEGIN
-import com.fasterxml.jackson.core.JsonProcessingException;
 import java.io.IOException;
 
 class App {
