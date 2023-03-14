b27bfb7c9e98ffd3feab688094c787a7ff37c380
@hexlet/cli <support@hexlet.io>
2023-03-14 16:48:29 +0300

    submit java-web-ru/deploy

---
+++ java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
@@ -0,0 +1,38 @@
+package exercise.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import static exercise.Data.getCompanies;
+
+public class CompaniesServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+                throws IOException, ServletException {
+
+        // BEGIN
+       List<String> companies = getCompanies();
+       PrintWriter out = response.getWriter();
+       String reqStr = request.getQueryString();
+       String param = request.getParameter("search");
+       String result = "";
+
+       if (param == "" || !reqStr.contains("search")) {
+           companies.forEach(out::println);
+       } else {
+           result = companies.stream()
+                   .filter(item -> item.contains(param))
+                   .collect(Collectors.joining("\n"));
+           out.print(result.equals("") ? "Companies not found" : result);
+       }
+        // END
+    }
+}
