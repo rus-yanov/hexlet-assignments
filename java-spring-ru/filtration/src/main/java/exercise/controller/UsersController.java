21dbcd4d9397c142670c26da471046ed864e68bb
@hexlet/cli <support@hexlet.io>
2023-05-11 21:39:31 +0300

    submit java-spring-ru/filtration

---
+++ java-spring-ru/filtration/src/main/java/exercise/controller/UsersController.java
@@ -0,0 +1,30 @@
+package exercise.controller;
+import exercise.model.User;
+import exercise.model.QUser;
+import exercise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// Зависимости для самостоятельной работы
+// import org.springframework.data.querydsl.binding.QuerydslPredicate;
+// import com.querydsl.core.types.Predicate;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // BEGIN
+    @GetMapping(path = "")
+    public Iterable<User> getUsersByName(@QuerydslPredicate(root = User.class) Predicate predicate) {
+        return userRepository.findAll(predicate);
+    }
+
+    // END
+}
+
